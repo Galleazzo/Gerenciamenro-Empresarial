d5ff73edb1d33c84b68c2e542bae8ed03d901086
Galleazzo <paulogalleazzo1@gmail.com>
2023-02-04 13:32:14 -0300

    alteracoes

--- src/main/java/br/com/paulo/gerenciamentoEmpresa/Gerenciamento/Empresas/controller/TipoEmpresaController.java
+++ src/main/java/br/com/paulo/gerenciamentoEmpresa/Gerenciamento/Empresas/controller/TipoEmpresaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.paulo.gerenciamentoEmpresa.Gerenciamento.Empresas.models.entity.TipoEmpresa;
@@ -21,12 +22,13 @@ public class TipoEmpresaController {
 	@Autowired
 	private TipoEmpresaRepo tipoEmpresaRepo;
 	
-	@RequestMapping("/Cadastradas")
+	@GetMapping("/Cadastradas")
 	public String todosTipo(Model model) {
 		List<TipoEmpresa> list = tipoEmpresaRepo.findAll();
 		model.addAttribute("lista", list);
 		return "TiposEmpresa";
 	}
+	
 	
 	
 }
