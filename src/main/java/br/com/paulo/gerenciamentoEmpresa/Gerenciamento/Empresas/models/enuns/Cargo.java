5a801beeae614cd67adf80442493a1629ac4fa78
Galleazzo <paulogalleazzo1@gmail.com>
2023-02-14 19:54:13 -0300

    tipos

--- src/main/java/br/com/paulo/gerenciamentoEmpresa/Gerenciamento/Empresas/models/enuns/Cargo.java
+++ src/main/java/br/com/paulo/gerenciamentoEmpresa/Gerenciamento/Empresas/models/enuns/Cargo.java
@@ -1,5 +1,15 @@
 package br.com.paulo.gerenciamentoEmpresa.Gerenciamento.Empresas.models.enuns;
 
 public enum Cargo {
-
+	
+	Desenvolvedor,
+	Faxineiro,
+	Gerente,
+	Diretor,
+	Administração,
+	Motorista,
+	Entregador,
+	Logistica,
+	Estágiario,
+	Treinee
 }
